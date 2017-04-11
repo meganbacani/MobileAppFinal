f0788736eced61152125209db050b2ff4321de38
tinacaili <tinacaili@github.com>
2017-04-11 15:19:08 -0500

    changed to viewcontroller

---
+++ LanguageLeapMobileAppFinal/LanguageLeapMobileAppFinal/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  LanguageLeapMobileAppFinal
+//
+//  Created by Tina Li on 4/11/17.
+//  Copyright © 2017 Megan Bacani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+}
